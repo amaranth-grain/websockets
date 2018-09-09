087f4c256dc58482c0e1bd593943676e2d2594eb
Christy Yau <abstruse.amaranth@gmail.com>
2018-09-09 12:08:39 -0700

    Simple websockets chat app

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//Set-up application
+var app = express();
+var server = app.listen(4000, function(){
+    console.log("Currently listening to requests on port 4000.");
+});
+
+//Static files
+app.use(express.static("public"));
+
+//Set-up socket
+var io = socket(server);
+
+//Call this function when connection is establishied.
+io.on('connection', function(socket){
+    console.log("Socket " + socket.id + " has established connection.");
+
+    //Handle chat event
+    socket.on("IM", function(data){
+        io.sockets.emit("IM", data);
+    });
+
+    //Handle feedback event
+    socket.on("feedback", function(data){
+        socket.broadcast("feedback", data);
+    });
+});
+
